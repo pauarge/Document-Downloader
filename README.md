33a2a76e5a97ed9d4a4fddb4bb477ec4534572a1
Pau Argelaguet <pau@outlook.com>
2014-12-08 23:23:23 +0100

    Some unfinished changes

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-#PDF Parser
+#Document Downloader
+
+As a university student, I get most of the studying stuff through the Internet. Most professors give me a URL where I can download all the required files to work (mostly, PDF theory files). The problem is that usually in that pages there are lots of files, and manually download all of them
+
+I did this simple script to massively download files from a webpage which has all the links in it.  
+
+##How it works
+
+
 
 ##Requirements
 
-PDF Parser runs on Python 2.7 (I'm working on a Python 3 version).
+This script runs on Python 2.7 (I'm working on a Python 3 version), so if you have Python installed in your computer, it should work! I've tested heavily on a Mac, but if you have a Linux/Windows machine, you shouldn't have any problem.
 
-You need Beautiful Soup to run it.
+You need Beautiful Soup to run it. The other packages we are using are bundled by default with any Python instalation.
--- parser.py
+++ parser.py
@@ -6,13 +6,20 @@ from urlparse import urljoin, urlsplit
 
 print "Please, write the URL where you want to download from"
 url = raw_input()
+print "What kind of file you wanna download? (type extension without the '.')"
+filetype = raw_input()
+print "Where do you wanna store the files? (write a relative path from where you are running this script, default is out/)"
+outpath = raw_input()
+if not outpath:
+    outpath = "out/"
+
 if url[:7] != 'http://' and url[:8] != 'https://':
         url = 'http://'+url
 page = urlopen(url).read()
 soup = bs(page)
 
 for link in soup.find_all('a'):
-    if link.get('href')[-4:] == '.pdf':
+    if link.get('href')[-4:] == '.'+filetype:
         down = urljoin(url, link.get('href'))
         split = urlsplit(down)
         filename = split.path.split("/")[-1]
@@ -20,6 +27,6 @@ for link in soup.find_all('a'):
         data = f.read()
         if not path.exists("out"):
             makedirs("out")
-        with open("out/"+filename, "wb") as code:
+        with open(+filename, "wb") as code:
             code.write(data)
             print "Downloaded " + filename
