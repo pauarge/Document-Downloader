ea22cd4d2318ccdf708c2602ace63bbed1a166be
Pau Argelaguet <pau@outlook.com>
2015-02-02 10:34:21 +0100

    Finished command line arguments

--- document-downloader.py
+++ document-downloader.py
@@ -1,34 +1,58 @@
+from bs4 import BeautifulSoup as bs
 from os import path, makedirs, system
 from urllib2 import urlopen
-from bs4 import BeautifulSoup as bs
 from urlparse import urljoin, urlsplit
-import time, sys, getopt, argparse
+import time
+import sys
+import argparse
+
+
+def parse_url(url):
+    if url[:7] != 'http://' and url[:8] != 'https://':
+        return 'http://'+url
+    else:
+        return url
+
+
+def parse_filetype(filetype):
+    if filetype[:1] != '.':
+        return '.'+filetype
+    else:
+        return filetype
 
 
 def main(argv):
 
-    outpath = 'out/'
-
     parser = argparse.ArgumentParser()
-    parser.add_argument('-o', '--output')
+    parser.add_argument('-o', '--outpath')
+    parser.add_argument('-u', '--url')
+    parser.add_argument('-f', '--filetype')
     args = parser.parse_args()
 
-    system('clear')
+    if args.outpath:
+        outpath = args.outpath
+    else:
+        outpath = 'out/'
 
-    print "Please, write the URL where you want to download from"
-    url = raw_input()
-    print "What kind of file you wanna download? (type extension without the '.')"
-    filetype = raw_input()
+    if args.url:
+        url = parse_url(args.url)
+    else:
+        print ("Please, write the URL where you want to download from")
+        url = parse_url(raw_input())
+
+    if args.filetype:
+        filetype = parse_filetype(args.filetype)
+    else:
+        print ("What kind of file you wanna download?")
+        filetype = parse_filetype(raw_input())
 
     count = 0
     start_time = time.time()
-    if url[:7] != 'http://' and url[:8] != 'https://':
-            url = 'http://'+url
     page = urlopen(url).read()
     soup = bs(page)
 
     for link in soup.find_all('a'):
-        if link.get('href')[-4:] == '.'+filetype:
+        if link.get('href')[-4:] == filetype:
             down = urljoin(url, link.get('href'))
             split = urlsplit(down)
             filename = split.path.split("/")[-1]
@@ -38,13 +62,14 @@ def main(argv):
                 makedirs(outpath)
             with open(outpath+filename, "wb") as code:
                 code.write(data)
-                print "Downloaded " + filename
+                print ("Downloaded " + filename)
                 count += 1
 
-    final_time = round(time.time()-start_time,2)
+    final_time = round(time.time()-start_time, 2)
 
-    print "\n"
-    print "Downlaoded "+str(count)+" files in "+str(final_time)+" seconds." 
+    print ("\n Downlaoded "+str(count)+" files in "+str(final_time)+" seconds.")
+
 
 if __name__ == '__main__':
+    system('clear')
     main(sys.argv[1:])
